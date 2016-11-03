ae6691849b41216398c6cf9f9c3a83e3ba0b1569
Pat-kun <jptlightx005@gmail.com>
2016-11-03 08:35:38 +0800

    Added windows

---
+++ SMSConcernSystem/SMSConcernSystem/Windows/Log-in.xaml.vb
@@ -0,0 +1,13 @@
+﻿Public Class Log_in
+
+    Private Sub btnLogin_Click(sender As Object, e As RoutedEventArgs) Handles btnLogin.Click
+        If txtUsrn.Text = "admin" And txtPssw.Password = "password" Then
+            MessageBox.Show("Successfully logged in!", "Log-in", MessageBoxButton.OK, MessageBoxImage.Information)
+            Dim mainWindow As New MainWindow
+            mainWindow.Show()
+            Me.Close()
+        Else
+            MessageBox.Show("Failed to log in!", "Log-in", MessageBoxButton.OK, MessageBoxImage.Exclamation)
+        End If
+    End Sub
+End Class
---
+++ SMSConcernSystem/SMSConcernSystem/Windows/Registration.xaml.vb
@@ -0,0 +1,6 @@
+﻿Public Class Registration
+
+    Private Sub Grid_Loaded(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+End Class
