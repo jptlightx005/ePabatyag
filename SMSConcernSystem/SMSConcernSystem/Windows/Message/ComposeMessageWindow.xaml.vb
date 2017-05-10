d2b0dd6043a1290cd9c78f8c4fb62a62ab2f3ddc
Pat-kun <jptlightx005@gmail.com>
2017-05-10 23:27:55 +0800

    Fixed compose message window

--- SMSConcernSystem/SMSConcernSystem/Windows/Message/ComposeMessageWindow.xaml.vb
+++ SMSConcernSystem/SMSConcernSystem/Windows/Message/ComposeMessageWindow.xaml.vb
@@ -5,6 +5,7 @@
         If recipients.Count > 0 Then
             txtRecipients.Text = ""
             For Each contact In recipients
+                txtRecipients.Text += String.Format("{0}, ", contact.contactNo)
             Next
             If txtRecipients.Text.Length > 0 Then
                 txtRecipients.Text = txtRecipients.Text.Substring(0, txtRecipients.Text.Length - 2)
