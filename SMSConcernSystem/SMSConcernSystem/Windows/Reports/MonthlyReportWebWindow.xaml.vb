88edaa5d1c3445a00b08ad9e069173aa0ae9741f
Pat-kun <jptlightx005@gmail.com>
2017-05-10 23:35:38 +0800

    Added a new Window

---
+++ SMSConcernSystem/SMSConcernSystem/Windows/Reports/MonthlyReportWebWindow.xaml.vb
@@ -0,0 +1,2 @@
+﻿Public Class MonthlyReportWebWindow
+End Class
--- SMSConcernSystem/SMSConcernSystem/Windows/Reports/MonthlyReportWindow.xaml.vb
+++ SMSConcernSystem/SMSConcernSystem/Windows/Reports/MonthlyReportWindow.xaml.vb
@@ -7,4 +7,15 @@ Public Class MonthlyReportWindow
         Dim tryMeh = HTMLReportPage.Replace("{0}", "Gotta touch this =)")
 
     End Sub
+
+    Private Sub btn_close_Click(sender As Object, e As RoutedEventArgs) Handles btn_close.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btn_print_Click(sender As Object, e As RoutedEventArgs) Handles btn_print.Click
+        Dim webReportWindow As New MonthlyReportWebWindow
+        webReportWindow.selectedMonth = TryCast(cmb_month.SelectedItem, ComboBoxItem).Content
+        Debug.Print("Selected month is {0}", webReportWindow.selectedMonth)
+        webReportWindow.ShowDialog()
+    End Sub
 End Class
